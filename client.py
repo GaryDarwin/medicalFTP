8481c4d3931ad6555b72c4b48b033c78e79c928b
britlecucumber <brittlecucumber@gmail.com>
2022-08-05 14:00:28 +0100

    Rearranged code into functions, removed anonymous

--- client.py
+++ client.py
@@ -1,12 +1,30 @@
+#importing required libraries
 import ftplib
 
-ftp = ftplib.FTP("127.0.0.1")
-ftp.login()
+#functon for FTP connection
+def ftp_connect():
+    #user input for server IP address
+    ftp_inp = input("Enter the IP address of the FTP Server: \n")
+    #ftp = ftplib.FTP("127.0.0.1:21")
+    ftp = ftplib.FTP(ftp_inp)
 
-print(ftp.getwelcome())
-print(ftp.pwd())
-ftp.dir()
-handle = open("medicaldata.csv", 'wb')
-ftp.retrbinary('RETR %s' % "medicaldata.csv", handle.write)
-ftp.cwd("/")
-ftp.quit()
+    #login to FTP server
+    ftp.login(user='user', passwd='12345')
+    return ftp
+
+def ftp_download(ftp):
+    #print(ftp.getwelcome())
+    #print(ftp.pwd())
+    ftp.dir()
+    handle = open("medicaldata.csv", 'wb')
+    ftp.retrbinary('RETR %s' % "medicaldata.csv", handle.write)
+    ftp.cwd("/")
+
+    
+
+def ftp_quit(ftp):
+    ftp.quit()
+
+ftp = ftp_connect()
+ftp_download(ftp)
+ftp_quit(ftp)
--- server.py
+++ server.py
@@ -4,13 +4,15 @@ from pyftpdlib.servers import FTPServer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.authorizers import DummyAuthorizer
 
+#the address of the server, also specifies the port
+#so local host, can only communicate with itself
 address = ("127.0.0.1",21)
 handler = FTPHandler
 authorizer = DummyAuthorizer()
 handler.authorizer = authorizer
 
 authorizer.add_user('user', '12345', './files', perm='elradfmwMT')
-authorizer.add_anonymous('./files')
+#authorizer.add_anonymous('./files')
 
 handler.banner = "Welcome to the server"
 
